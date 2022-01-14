51a2d2cc04b0eb423bf4f5216bbffd8a68582d52
Risky Nugraha <muhammadrisky83@gmail.com>
2022-01-14 23:34:57 +0700

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 4321;
+const multerCheck = require('./middlewares/multer');
+
+const app = express();
+
+app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, '/images')));
+
+app.post('/upload/image', multerCheck(), (req, res) => {
+  const file = req.file;
+
+  if (!file) {
+    return res.status(400).json({
+      msg: 'Wrong File input',
+    });
+  }
+
+  res.send('image uploaded');
+});
+
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
