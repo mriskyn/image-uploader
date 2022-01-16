863d91c477dce7125c81652b222ec187348d62b1
Risky Nugraha <muhammadrisky83@gmail.com>
2022-01-16 20:33:38 +0700

    manage index file

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 4321;
-const multerCheck = require('./middlewares/multer');
+
+const { sequelize } = require('./models');
+const authRoutes = require('./routes/authRoutes')
+const imageRoutes = require('./routes/imageRoutes')
 
 const app = express();
 
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, '/images')));
 
-app.post('/upload/image', multerCheck(), (req, res) => {
-  const file = req.file;
+app.use('/api/v1', authRoutes)
+app.use('/api/v1', imageRoutes)
 
-  if (!file) {
-    return res.status(400).json({
-      msg: 'Wrong File input',
-    });
+async function main() {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync({ alter: true });
+    console.log('DB Connected');
+  } catch (err) {
+    console.log(err, 'err DB');
   }
 
-  res.status(201).json({ file });
-});
+  app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+}
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+main();
