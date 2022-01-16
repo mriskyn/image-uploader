d943840228ff96728d3557dcdf707596596d4596
Risky Nugraha <muhammadrisky83@gmail.com>
2022-01-16 20:49:39 +0700

    db host conf

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: '127.0.0.1',
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     logging: false
